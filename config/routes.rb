08abf11453a7fd702b72931710056ca4578e8b25
ChrisDubs <chdubs1@gmail.com>
2017-02-02 16:11:42 +0200

    test check

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  resource :artists, only: :index
-  resource :tickets, only: :index
-
   root 'home#index'
 
+  match '/artists',      to: 'artists#index',     via: 'get'
+  match '/tickets',      to: 'tickets#index',     via: 'get'
   match '/about_us',     to: 'home#about_us',     via: 'get'
   match '/contact_us',   to: 'home#contact_us',   via: 'get'
   match '/cooperation',  to: 'home#cooperation',  via: 'get'
